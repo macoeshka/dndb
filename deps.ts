3ac5f56a23369987e4f6aa009bea8fa7142ffeb9
Egor Taranin <egor.taranin@gmail.com>
2022-10-20 15:07:16 +0300

    fix bufio

--- deps.ts
+++ deps.ts
@@ -2,9 +2,9 @@ import updater from "https://cdn.skypack.dev/mongobj"
 import { matches }  from 'https://raw.githubusercontent.com/denyncrawford/safe-filter/master/dist/index.js'
 import project from 'https://raw.githubusercontent.com/denyncrawford/mongo-project.node/master/dist/bundle.js'
 import { EventEmitter } from "https://deno.land/std/node/events.ts";
-import { BufReader } from "https://deno.land/std/io/bufio.ts";
-import { existsSync } from "https://deno.land/std/fs/mod.ts";
-import { resolve, dirname, join } from 'https://deno.land/std/path/mod.ts';
+import { BufReader } from "https://deno.land/std@0.158.0/io/bufio.ts";
+import { existsSync } from "https://deno.land/std@0.158.0/fs/mod.ts";
+import { resolve, dirname, join } from 'https://deno.land/std@0.158.0/path/mod.ts';
 
 export { 
   updater,
