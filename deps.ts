42efd146998171193b40cd6be3b201ab18343d6e
macoeshka <macoeshka@gmail.com>
2023-06-16 15:20:03 +0300

    0.3.3.2

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 import updater from "https://cdn.skypack.dev/mongobj"
 import { matches }  from 'https://raw.githubusercontent.com/denyncrawford/safe-filter/master/dist/index.js'
 import project from 'https://raw.githubusercontent.com/denyncrawford/mongo-project.node/master/dist/bundle.js'
-import { EventEmitter } from "https://deno.land/std/node/events.ts";
+import { EventEmitter } from "https://deno.land/std@0.172.0/node/events.ts";
 import { BufReader } from "https://deno.land/std@0.158.0/io/bufio.ts";
 import { existsSync } from "https://deno.land/std@0.158.0/fs/mod.ts";
 import { resolve, dirname, join } from 'https://deno.land/std@0.158.0/path/mod.ts';
